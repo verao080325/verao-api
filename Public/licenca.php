8f645627067931efb3a38c3fca7a7753281de857
Verao <softwareverao@gmail.com>
2025-05-08 14:47:38 +0000

    outra atualizacao

--- Public/licenca.php
+++ Public/licenca.php
@@ -8,7 +8,7 @@ $data = json_decode(file_get_contents("php://input"), true);
 if ($data) {
     // Exemplo de geração de chave com base nos dados (você pode melhorar isso com algoritmos mais seguros)
     $chave_gerada = md5(uniqid($data['id'], true));  // Gera uma chave simples (melhore conforme necessário)
-
+var_dump($data);
     // Retorna a chave gerada para a aplicação local
     echo json_encode([
         'mensagem' => 'Licença gerada com sucesso!',
